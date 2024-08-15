532770b94f3d03235e720db13341ce990b4a166e
Muhammed Ahmed Modibbo <modibbomuhammed@gmail.com>
2024-08-15 15:48:51 +0100

    GameSetup

---
+++ app.js
@@ -0,0 +1,11 @@
+// Ludo Game Play
+
+const allPlayers = ["blue","red","green","yellow"];
+
+const numberOfPlayer = Number(prompt('Choose Number Of Players between 1 - 4'));
+
+let currentPlayers = allPlayers.slice(0,numberOfPlayer);
+
+function play(){
+    
+}
